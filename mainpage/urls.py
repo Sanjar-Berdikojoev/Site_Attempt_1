3cb4676bad44b3e923479c81830399bd05d1a09e
Sancho <sanjarberdikojoev@gmail.com>
2022-12-06 17:41:38 +0600

    Get_ID_system_New_Update

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.mainpage_all, name='home'),
-    path('<int:id>/', views.extended_info, name=''),
-    # path('', views.courses, name=''),
-    # path('', views.blogs, name=''),
-    # path('', views.traffic_laws, name=''),
+    path('<int:id>/', views.instructor_extended_info, name=''),
+    path('<int:id>/', views.course_extended_info, name=''),
+    path('<int:id>/', views.blogs_extended_info, name=''),
+    path('<int:id>/', views.traffic_laws_extended_info, name=''),
 ]
--- mainpage/views.py
+++ mainpage/views.py
@@ -13,28 +13,19 @@ def mainpage_all(request):
         'traffic_laws': traffic_laws,
     }
     return render(request, 'index.html', context)
-# Create your views here.
 
-def extended_info(request, id):
+def instructor_extended_info(request, id):
     instructor = models.Instructor.objects.get(id=id)
+    return render(request, 'instructor_extended_info', {'instructor': instructor})
+
+def course_extended_info(request, id):
     course = models.Course.objects.get(id=id)
+    return render(request, 'course_extended_info', {'course': course})
+
+def blogs_extended_info(request, id):
     blogs = models.Blog.objects.get(id=id)
+    return render(request, 'blogs_extended_info', {'blogs': blogs})
+
+def traffic_laws_extended_info(request, id):
     traffic_laws = models.Traffic_Law.objects.get(id=id)
-    context = {
-        'instructor': instructor,
-        'course': course,
-        'blogs': blogs,
-        'traffic_laws': traffic_laws,
-    }
-    return render(request, 'extended_info.html', context)
-# def courses(request):
-#     course = models.Course.objects.all()
-#     return render(request, 'index.html', {'course': course})
-#
-# def blogs(request):
-#     blogs = models.Blog.objects.all()
-#     return render(request, 'index.html', {'blogs': blogs})
-#
-# def traffic_laws(request):
-#     traffic_laws = models.Traffic_Law.objects.all()
-#     return render(request, 'index.html', {'traffic_laws': traffic_laws})
+    return render(request, 'traffic_laws_extended_info', {'traffic_laws': traffic_laws})
